19bb1128be0c60f37a4af85800e5dd67622976f1
JackOliCode <jack.v.oliver@gmail.com>
2023-07-27 17:00:09 +0100

    Create destination.py

---
+++ ex1.3/destination.py
@@ -0,0 +1,13 @@
+destination = input("Would you like to travel to London, Manchester or Leeds?: ")
+
+if destination == "London":
+    print("Enjoy your stay in" + destination)
+
+elif destination == "Manchester":
+    print("Enjoy your stay in" + destination)
+
+elif destination == "Leeds":
+    print("Enjoy your stay in" + destination)
+
+else:
+    print("Oops, that destination is not currently available.")
